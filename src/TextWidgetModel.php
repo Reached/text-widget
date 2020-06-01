41fa9fda04274d6102d46677f8a8ae043942e871
Casper Sørensen <casper@ulobby.dk>
2020-06-01 17:02:12 +0200

    Clear cache

--- src/TextWidgetModel.php
+++ src/TextWidgetModel.php
@@ -3,10 +3,31 @@
 namespace Reached\TextWidget;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 
 class TextWidgetModel extends Model
 {
     protected $fillable = ['block_name', 'text'];
 
     protected $table = 'text_widgets';
+
+    private static function clearCache() {
+        if(app()->environment('production')) {
+            Cache::flush();
+        }
+    }
+
+    public function boot() {
+        self::created(function() {
+            $this->clearCache();
+        });
+
+        self::updated(function() {
+            $this->clearCache();
+        });
+
+        self::deleted(function() {
+            $this->clearCache();
+        });
+    }
 }
